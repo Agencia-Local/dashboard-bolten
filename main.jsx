b9329454e69608388a7409578e590028ec9b28bf
Agencia-Local <agencialocaldigital@gmail.com>
2026-06-24 16:28:47 -0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import DashboardBolten from './App'
+import DashboardBolten from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
